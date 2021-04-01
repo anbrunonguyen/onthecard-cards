280e83193ee39f12c7401ee4343f708985740e07
Nguyen Thanh An <an.nguyen@techx.vn>
2021-04-01 22:08:56 +0700

    happy hacking

---
+++ src/view/components/CoverPhoto/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import * as classes from './style.module.css';
+
+function CoverPhoto() {
+  return (
+    <div className={classes.CoverPhoto}>
+      <h1>Your code here</h1>
+    </div>
+  );
+}
+
+export default CoverPhoto;
