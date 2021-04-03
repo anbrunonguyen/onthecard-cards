a1fed67f7aae95208b8c5ba8f0b6008a0fe780ee
Nguyen Thanh An <an.nguyen@techx.vn>
2021-04-03 11:38:23 +0700

    Add AOS and demo card

---
+++ src/view/components/Card/Style1/index.js
@@ -0,0 +1,7 @@
+import React from 'react';
+import * as classes from './style.module.css';
+function Style1() {
+  return <div data-aos='fade-down' className={classes.Style1}></div>;
+}
+
+export default Style1;
--- src/view/components/Card/index.js
+++ src/view/components/Card/index.js
@@ -1,7 +1,17 @@
 import React from 'react';
+import { CardStye } from '../../../core/constant';
+import Style1 from './Style1';
 
-function Card() {
-  return <div></div>;
+function Card({ style }) {
+  let renderItem;
+  switch (style) {
+    case CardStye.S1:
+      renderItem = <Style1 />;
+      break;
+    default:
+      renderItem = <Style1 />;
+  }
+  return renderItem;
 }
 
 export default Card;
