2666e24df3ca53ef02d8b78fc59c378f52222b50
Nguyễn Văn Kiệt <Vankiet.kn78@gmail.com>
2021-04-06 21:46:26 +0700

    Nothing

--- src/view/components/CoverPhoto/index.js
+++ src/view/components/CoverPhoto/index.js
@@ -1,10 +1,23 @@
-import React from 'react';
-import * as classes from './style.module.css';
+import React from "react";
+import * as classes from "./style.module.css";
 
 function CoverPhoto() {
   return (
     <div className={classes.CoverPhoto}>
-      <h1>Your code here</h1>
+      <div className={classes.GridContainer}>
+        <div className={classes.Item1}>
+          <div className={classes.BackGroundImg}>
+            <a href="https://aboutme.vn/namecard/phung-minh-duc-anh/">
+              <img
+                alt=""
+                src="https://aboutme.vn/wp-content/uploads/2021/01/cover-1024x576.jpg"
+                width="100%"
+                height="100%"
+              />
+            </a>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
